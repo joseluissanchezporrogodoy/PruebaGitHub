ea3c21d15bec6bcd755132d50953eff0ea3344ef
Jose Luis <joseluis.sanchezporro.godoy@gmail.com>
2025-02-17 10:26:38 +0100

    Initial Commit

---
+++ PruebaGitHub/PruebaGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaGitHubApp.swift
+//  PruebaGitHub
+//
+//  Created by jose luis sanchez-porro godoy on 17/2/25.
+//
+
+import SwiftUI
+
+@main
+struct PruebaGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
