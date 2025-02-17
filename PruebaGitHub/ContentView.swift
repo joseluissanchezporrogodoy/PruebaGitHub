1b0be3534be171d15527fa1ee9a7c4643770b0a3
Jose Luis <joseluis.sanchezporro.godoy@gmail.com>
2025-02-17 11:22:09 +0100

    🐛 fix: resolve issue causing incorrect data rendering
    https://jira.si.orange.es/browse/TVAPPLE-233
    
    Changes in.....

--- PruebaGitHub/ContentView.swift
+++ PruebaGitHub/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hola Mundo!!")
+            Text("Hola Mundo de nuevo!!")
         }
         .padding()
     }
